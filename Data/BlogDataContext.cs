9a021d3808ec352e59b5cd656993da1ac3ef0005
CVieiraSantos <cadu.vieira2276@gmail.com>
2022-07-22 12:15:45 -0300

    Alterando a string de conexao com o banco de dados SQLServer

--- Data/BlogDataContext.cs
+++ Data/BlogDataContext.cs
@@ -6,7 +6,7 @@ namespace Blog.Data
     public class BlogDataContext : DbContext
     {
         protected override void OnConfiguring(DbContextOptionsBuilder options)
-        => options.UseSqlServer(@"Server=localhost,1433,Database=Blog;User ID=ESTACIOACAD\201907300961;Integrated Security=true");
+        => options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Blog;Integrated Security=true");
         
 
         public DbSet<Category> Categories { get; set; }
