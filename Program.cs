59279829dab71438cc0602b3a8e831d4b0d7ca23
CVieiraSantos <cadu.vieira2276@gmail.com>
2022-07-22 15:51:50 -0300

    Criacao, deletao, alteracao e leitura

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using System.Security.Cryptography.X509Certificates;
+using System;
 using Blog.Data;
 using Blog.Models;
+using System.Linq;
 
 namespace Blog
 {
@@ -9,10 +11,37 @@ namespace Blog
         static void Main(string[] args)
         {
             using(var context = new BlogDataContext())
-            {
-                var tag = new Tag {Name = "ASP.NET", Slug = "aspnet"};
-                context.Tags.Add(tag);
-                context.SaveChanges();
+            {   
+                //Create
+                //  var tag = new Tag {Name = "ASP.NET", Slug = "aspnet"};
+                //  context.Tags.Add(tag);
+                //  context.SaveChanges();
+
+                //  var tag2 = new Tag {Name = "ASP.NET MVC", Slug = "aspnet mvc"};
+                //  context.Tags.Add(tag2);
+                //  context.SaveChanges();
+
+                //Update
+                // var tag = context.Tags.FirstOrDefault(x=> x.Id == 1);
+                // tag.Name = ".NET";
+                // tag.Slug = "dotnet";
+                // context.Update(tag);
+                // context.SaveChanges();
+
+                //Delete
+                // var tag = context.Tags.FirstOrDefault(x=> x.Id == 1);
+                // context.Remove(tag);
+                // context.SaveChanges();
+
+                var tags = context
+                .Tags
+                .ToList();                
+
+                foreach (var item in tags)
+                {
+                    Console.WriteLine(item.Name);
+                }
+
             }
         }
     }
